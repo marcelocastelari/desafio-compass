253441aa79142ebc3b49896a5d7012dd9bb25a20
marcelocastelari <marcelocastelari@gmail.com>
2022-09-28 16:16:23 -0300

    update creator link in footer

--- components/indexFooter.vue
+++ components/indexFooter.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="flex justify-center items-center">
-       <p class="font-medium"> Feito com <span class="text-red-500">&hearts;</span> por <nuxt-link to="https://www.linkedin.com/in/marcelocastelari/" class="hover:text-green-400">Marcelo Castelari</nuxt-link> </p> 
+       <p class="font-medium"> Feito com <span class="text-red-500">&hearts;</span> por <a class="hover:text-green-400" href="https://www.linkedin.com/in/marcelocastelari/" target="_blank">Marcelo Castelari</a> </p> 
     </div>
 </template>
